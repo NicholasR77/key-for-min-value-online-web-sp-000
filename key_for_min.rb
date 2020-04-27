7fd315d3dbad2f988ee5e4b99ca1bb1921409a0c
Nicholas Robbins <nl7robbins@icloud.com>
2020-04-27 18:20:41 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,10 +2,10 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  key = nil
-  value = nil
+  var_key = nil
+  var_value = nil
 
   hash.each do |key, value|
-      if (
+      if (value = nil || value < )
   end
 end
