f10b7606918079e87e0ea372d3e19907813673fa
Nicholas Robbins <nl7robbins@icloud.com>
2020-04-27 18:22:13 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,7 +5,7 @@ def key_for_min_value(name_hash)
   var_key = nil
   var_value = nil
 
-  hash.each do |key, value|
+  name_hash.each do |key, value|
       if (var_value = nil || var_value < value)
         var_value = value
         var_key = key
